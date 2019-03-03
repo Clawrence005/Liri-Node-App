b795e76405452c59139b53f2b6d8f10237112297
colin <clawrence005@yahoo.com>
2019-03-02 20:03:52 -0600

    the log function works, spotify works, and movies work. the band and do-this-thing needs to be worked on.

--- liri.js
+++ liri.js
@@ -42,12 +42,16 @@ function switchCase() {
 
         case "do-what-it-says":
             readTxtFile();
-            console.log("do-what-it-says" + parameter);
+            console.log("do-what-it-says");
             break;
 
+        // case "do-what-it-says":
+        //     spotifySong(parameter);
+        //     console.log("spotify-this-song" + parameter);
+        //     break;
+
         default:
-            // logIt("Invalid Instruction");
-            console.log("default")
+            console.log("Invalid Instruction. Refer to readMe for instructions")
             break;
 
     }
@@ -55,20 +59,54 @@ function switchCase() {
 
 function bandsInTown(parameter) {
 
-    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    // // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
     var queryURL = "https://rest.bandsintown.com/artists/" + parameter + "?app_id=codingbootcamp";
-    request(queryURL, function (error, response, body) {
-        console.log("band1");
 
-        if (!error && response.statusCode === 200) {
-        } else {
-            console.log("band2");
-        }
-    }).then(function (response) {
-        var JS = JSON.parse(body);
-        console.log(JS)
-    })
+    //i couldnt get this api to work exactly
+    // var queryURL = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
+
+    // axios.get(queryURL, function (error, response, body) {
+    //     console.log("band1");
+
+    //     if (!error && response.statusCode === 200) {
+    //     } else {
+    //         console.log("band2");
+    //     }
+    // }).then(function (response) {
+    //     var band = JSON.parse(response);
+    //     console.log(response);
+    //     console.log(band);
+    // })
+
+    axios.get(queryURL)
+        .then(function (response) {
+            var jsonData = response.data;
+
+
+            // var JS = JSON.parse(body);
+            // // for (i = 0; i < JS.length; i++)
+
+            // for (i = 0; i < jsonData.length; i++) {
+            //     console.log("1" + response.data);
+            //     console.log("2" + JS);
+            //     console.log("3" + jsonData);
+            // }
+            var bandLog =
+                ` 
+Bands In Town      
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~            
+Artist Name: ${response.data.name}
+Upcoming Events: ${response.data.upcoming_event_count}
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        `;
+            fs.appendFile('log.txt', bandLog, function (err) {
+                if (err) throw err;
+                console.log(bandLog);
+            })
+        });
 }
+
 //////////
 function spotifySong(parameter) {
     // var search = parameter;
@@ -88,12 +126,31 @@ function spotifySong(parameter) {
             console.log('Error occurred: ' + error);
             return;
         } else {
-            console.log("\n-----------------------begin spotify----------------------------\n");
-            console.log("Artist: " + data.tracks.items[0].artists[0].name);
-            console.log("Song: " + data.tracks.items[0].name);
-            console.log("Preview: " + data.tracks.items[3].preview_url);
-            console.log("Album: " + data.tracks.items[0].album.name);
-            console.log("\n----------------------end spotify-----------------------------\n");
+            // this format is pushing blank space?????
+            // var spotifyLog2 = [
+            //     console.log("\n-----------------------begin spotify----------------------------\n"),
+            //     console.log("Artist: " + data.tracks.items[0].artists[0].name),
+            //     console.log("Song: " + data.tracks.items[0].name),
+            //     console.log("Preview: " + data.tracks.items[3].preview_url),
+            //     console.log("Album: " + data.tracks.items[0].album.name),
+            //     console.log("\n----------------------end spotify-----------------------------\n"),
+            // ].join("\n\n");
+
+            var spotifyLog =
+
+                ` 
+Spotify     
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~            
+Artist Name: ${data.tracks.items[0].artists[0].name},
+Language: ${data.tracks.items[0].name},
+Plot: ${ data.tracks.items[3].preview_url},
+Actors: ${data.tracks.items[0].album.name},
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        `;
+            fs.appendFile('log.txt', spotifyLog, function (err) {
+                if (err) throw err;
+                console.log(spotifyLog);
+            });
         }
     });
 
@@ -112,38 +169,99 @@ function movieInfo(parameter) {
     }
 
 
-    request(queryUrl, function (error, res, body) {
-        var movieObject = JSON.parse(body);
-        // console.log(movieObject);
-
+    request(queryUrl, function (error, results, body) {
+        var movieData = JSON.parse(body);
         if (error) {
-            // var movieResults =
             console.log('Error occurred: ' + error);
             return;
         }
         else {
-            console.log("\n-----------------------begin movie----------------------------\n");
-            console.log("Title :" + movieObject.Title);
-            console.log("Year :" + movieObject.Year);
-            console.log("imdbRating :" + movieObject.imdbRating);
-            console.log("Rotten Tomatoes Rating :" + movieObject.Ratings[1].Value);
-            console.log("Country :" + movieObject.Country);
-            console.log("Language :" + movieObject.Language);
-            console.log("Plot :" + movieObject.Plot);
-            console.log("Actors :" + movieObject.Actors);
-            console.log("\n--------------------------end movie-------------------------\n");
+            var movieLog =
+                `
+OMBD Movie Database
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~              
+Movie: ${movieData.Title}
+Year: ${movieData.Year}
+imdbRating: ${movieData.imdbRating}
+Rotten Tomatoes Rating: ${movieData.Ratings[1].Value}
+Rotten Tomatoes Rating: ${movieData.Country}
+Language: ${movieData.Language}
+Plot: ${movieData.Plot}
+Actors: ${movieData.Actors}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            `;
+            // this format wont log???
+            // var movieLog2 = [
+            //     console.log("\n-----------------------begin movie----------------------------\n"),
+            //     console.log("Title :" + movieData.Title),
+            //     console.log("Year :" + movieData.Year),
+            //     console.log("imdbRating :" + movieData.imdbRating),
+            //     console.log("Rotten Tomatoes Rating :" + movieData.Ratings[1].Value),
+            //     console.log("Country :" + movieData.Country),
+            //     console.log("Language :" + movieData.Language),
+            //     console.log("Plot :" + movieData.Plot),
+            //     console.log("Actors :" + movieData.Actors),
+            //     console.log("\n--------------------------end movie-------------------------\n"),
+            // ].join("\n\n");
+            // console.log(movieData);
+            fs.appendFile('log.txt', movieLog, function (err) {
+                if (err) throw err;
+                // console.log("response =" + results);
+                console.log("response = " + movieLog);
+            });
         }
     });
 };
 ///////////
 function readTxtFile(parameter) {
-    fs.readFile("random.txt", "utf8", function (error, data) {
-
+    fs.readFile("random.txt", "utf8", function (error, contents) {
+        var newData2 = "";
         if (error) {
             return console.log(error);
         }
-        console.log("random : " + parameter);
+        var data = contents.split(' ').join("");
+        var newData = data.split('"').join('');
+        newData2 = newData.split(',').join(' ');
+        // var newData3 = newData2.slice(0, 9);
+        console.log("1 : " + data);
+        console.log("2 : " + newData);
+        console.log("3 : " + newData2);
+        // i can return spotify-this-song IWantitThatWay
+        // need to figure out how to make this a command in the spotify function 
+        // i will probably need to change some architecture.
+
     });
 }
+//     var searchTrack = data.split(",").join(" ");
+
+//     spotify.search({
+//         type: 'track',
+//         query: searchTrack
+//     }, function (error, data) {
+//         if (error) {
+//             console.log('Error occurred: ' + error);
+//             return;
+//         } else {
+
+//             var spotifyLog =
+
+//                 ` 
+//     Spotify     
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~            
+//     Artist Name: ${data.tracks.items[0].artists[0].name},
+//     Language: ${data.tracks.items[0].name},
+//     Plot: ${ data.tracks.items[3].preview_url},
+//     Actors: ${data.tracks.items[0].album.name},
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//             `;
+//             fs.appendFile('log.txt', spotifyLog, function (err) {
+//                 if (err) throw err;
+//                 console.log(spotifyLog);
+//             });
+//         }
+//     });
+
+// }
+
 //////////
 switchCase();
